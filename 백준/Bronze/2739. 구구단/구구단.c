212d2882ed6db6dae79a445ec44017413725373c
COOLHO <coolho123@naver.com>
2023-01-27 18:46:21 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main()
+{    
+	int n;
+	scanf("%d", &n);
+
+	 
+	for (int i = 1; i<10; i++  )
+	{
+		printf("%d * %d = %d\n",n, i ,n*i);
+	}
+	
+
+
+	
+
+}
