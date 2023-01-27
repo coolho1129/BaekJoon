ad6b6eb67683f2baf18a261cd0cb07f30b343981
COOLHO <coolho123@naver.com>
2023-01-27 18:38:14 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main()
+{
+	int N, i, j;
+
+	scanf("%d", &N);
+
+
+	for (i = 1; i <= N; i++)
+	{
+		for (j = N; j >= i; j--)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+
+
+
+}
