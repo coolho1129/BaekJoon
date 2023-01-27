c1a53b94977e8bf5b1f9c9e8c20b2718e59c4dd0
COOLHO <coolho123@naver.com>
2023-01-27 18:59:27 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    while (getline(cin, str))
+    {
+
+        cout << str << "\n";
+    }
+
+    return 0;
+}
