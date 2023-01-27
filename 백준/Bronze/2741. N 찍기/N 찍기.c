83db78c00c15e1f67ea56695b21c3b017c41b8fb
COOLHO <coolho123@naver.com>
2023-01-27 18:46:55 +0900

    [Bronze V] Title: N 찍기, Time: 16 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main()
+{    
+	int n;
+	scanf("%d", &n);
+	
+	 
+	for (int i = 1; i<=n; i++ )
+	{
+		printf("%d\n", i);
+		
+		}
+	
+
+	
+	return 0;
+
+	
+
+}
