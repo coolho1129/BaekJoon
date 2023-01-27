daf7f1fe9bdbdbc96881cf515cf5278320c5ede5
COOLHO <coolho123@naver.com>
2023-01-27 19:01:02 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+
+int main() {
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	return 0;
+}
