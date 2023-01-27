0f084a70662eb571ebc63efec64a35e8967c893f
COOLHO <coolho123@naver.com>
2023-01-27 18:30:13 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+
+
+	
+int main()
+{
+	long long x;
+	int n, m,k = 1,sum=0;
+
+	scanf("%lld", &x);
+	if (x == 1)
+		printf("1/1");
+	else
+	{
+		for (int i = 1;; i++)
+		{
+			if (i * (i + 1) / 2 < x && x <=(i + 1) * (i + 2) / 2)
+			{
+				n = i;
+				break;
+
+			}
+		}
+		sum = (n+1) * n / 2;
+		int L = x - sum;
+		//printf("%d %d %d\n", n, sum, L);
+		
+		if (n % 2 == 1)
+		{
+			m = n + 1;
+			
+			for (int j = 1; j < L; j++)
+			{
+				k++, m--;
+			}
+			printf("%d/%d", k,m );
+		}
+		else
+		{
+			m = n + 1;
+			for (int j = 1; j < L; j++)
+			{
+				k++, m--;
+			}
+			printf("%d/%d ", m,k);
+
+
+		}
+
+	}
+}
