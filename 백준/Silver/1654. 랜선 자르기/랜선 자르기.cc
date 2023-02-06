5cac16783fe8e1842aa73848cf87a6d359ab1b01
COOLHO <coolho123@naver.com>
2023-02-07 02:36:04 +0900

    Update 랜선 자르기.cc

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -5,24 +5,29 @@ using namespace std;
 
 ll binarySearch(ll *lan, ll start, ll end, ll size, ll find)
 {
-    ll mid = (start + end) / 2;
-    ll sum = 0;
-    for (ll i = 0; i < size; i++)
+    ll result = 0;
+    while (start <= end)
     {
-        sum += lan[i] / mid;
-    }
-    if (sum >= find)
-    {
-        if (start == mid)
-            return mid;
+
+        ll mid = (start + end) / 2;
+        ll sum = 0;
+        for (ll i = 0; i < size; i++)
+        {
+            sum += lan[i] / mid;
+        }
+
+        if (sum >= find)
+        {
+            result = mid;
+            start = mid + 1;
+        }
         else
-            return binarySearch(lan, mid, end, size, find);
+        {
+            end = mid - 1;
+        }
     }
-    else
-    {
-     
-            return binarySearch(lan, start, mid, size, find);
-    }
+
+    return result;
 }
 
 int main()
@@ -30,19 +35,18 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    ll N, K,max=0;
+    ll N, K, max = 0;
 
     cin >> K >> N;
-    // cout << K << N;
 
     ll *lan = new ll[K];
     for (ll i = 0; i < K; i++)
     {
 
         cin >> lan[i];
-        if(max<lan[i])
-            max=lan[i];
+        if (max < lan[i])
+            max = lan[i];
     }
 
-   cout << binarySearch(lan, 1, max+1, K, N);
+    cout << binarySearch(lan, 1, max, K, N);
 }
