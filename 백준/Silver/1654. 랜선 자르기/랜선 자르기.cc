29998c9ab9819cee3efac7dc13a59dca1c1190cf
COOLHO <coolho123@naver.com>
2023-02-07 02:59:39 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -5,29 +5,26 @@ using namespace std;
 
 ll binarySearch(ll *lan, ll start, ll end, ll size, ll find)
 {
-    ll result = 0;
-    while (start <= end)
+    ll mid = (start + end) / 2;
+    ll sum = 0;
+    
+    for (ll i = 0; i < size; i++)
     {
-
-        ll mid = (start + end) / 2;
-        ll sum = 0;
-        for (ll i = 0; i < size; i++)
-        {
-            sum += lan[i] / mid;
-        }
-
-        if (sum >= find)
-        {
-            result = mid;
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
+        sum += lan[i] / mid;
     }
-
-    return result;
+    
+    if(start>end)
+        return mid;
+    
+    if (sum >= find)
+    {  
+      return binarySearch(lan, mid+1, end, size, find);
+    }
+    else
+    {
+     
+            return binarySearch(lan, start, mid-1, size, find);
+    }
 }
 
 int main()
@@ -35,18 +32,19 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    ll N, K, max = 0;
+    ll N, K,max=0;
 
     cin >> K >> N;
+    // cout << K << N;
 
     ll *lan = new ll[K];
     for (ll i = 0; i < K; i++)
     {
 
         cin >> lan[i];
-        if (max < lan[i])
-            max = lan[i];
+        if(max<lan[i])
+            max=lan[i];
     }
 
-    cout << binarySearch(lan, 1, max, K, N);
+   cout << binarySearch(lan, 1, max, K, N);
 }
