5377d48c2a93df8fe5f9b42e1b6fd8c2a84ec076
COOLHO <coolho123@naver.com>
2023-02-07 00:58:39 +0900

    [Silver III] Title: 통계학, Time: 108 ms, Memory: 5052 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.c
+++ 백준/Silver/2108. 통계학/통계학.c
@@ -1,112 +1,109 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include<math.h>
-#define SWAP(a,b,t) {t=a;a=b;b=t;}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#define SWAP(a, b, t) \
+    {                 \
+        t = a;        \
+        a = b;        \
+        b = t;        \
+    }
 
+int num[500001] = {0}, negative[4001] = {0}, positive[4001] = {0}, mode[500001];
 
+void quicksort(int left, int right, int arry[])
+{
+    int i, j, pivot, temp;
 
-int num[500001] = { 0 },negative[4001] = { 0 }, positive[4001] = { 0 }, mode[500001];
+    if (left < right)
+    {
+        i = left;
+        j = right + 1;
+        pivot = arry[left];
 
-void quicksort(int left, int right, int arry[]) {
-	int i, j, pivot, temp;
+        do
+        {
+            do
+                i++;
+            while (arry[i] < pivot);
+            do
+                j--;
+            while (arry[j] > pivot);
 
-	if (left < right) {
-		i = left;
-		j = right + 1;
-		pivot = arry[left];
+            if (i < j)
+                SWAP(arry[i], arry[j], temp);
 
-		do {
-			do i++; while (arry[i] < pivot);
-			do j--; while (arry[j] > pivot);
+        } while (i < j);
 
-			if (i < j)
-				SWAP(arry[i], arry[j], temp);
-
-		} while (i < j);
-
-		SWAP(arry[left], arry[j], temp);
-		quicksort(left, j - 1, arry);
-		quicksort(j + 1, right, arry);
-	}
-
+        SWAP(arry[left], arry[j], temp);
+        quicksort(left, j - 1, arry);
+        quicksort(j + 1, right, arry);
+    }
 }
 
+int findmode(int *postive, int *negative)
+{
+    int index = 0, max = 0;
 
+    for (int i = 0; i < 4001; i++)
+    {
+        if (max < postive[i])
+        {
+            max = postive[i];
+        }
 
-int findmode(int* postive, int* negative) {
-	int index = 0, maxP, maxN, Max;
+        if (i != 0 && max < negative[i])
+        {
 
-	maxP = postive[0];
-	maxN = negative[1];
+            max = negative[i];
+        }
+    }
 
-	for (int i = 0; i < 4001; i++) {
-		if (maxP < postive[i]) {
-			maxP = postive[i];
-		}
+    for (int i = 0; i < 4001; i++)
+    {
+        if (max == postive[i])
+        {
+            mode[index++] = i;
+        }
+        if (i != 0 && max == negative[i])
+            mode[index++] = -i;
+    }
 
-		if (i!=0&&maxN < negative[i]) {
+    quicksort(0, index - 1, mode);
 
-			maxN = negative[i];
-		}
-	}
-	Max = (maxN > maxP ? maxN : maxP);
+    if (index != 1)
+    {
+     
 
-	for (int i = 0; i < 4001; i++) {
-		if (Max == postive[i]) {
-			mode[index++] = i;
-
-		}
-		if (i!=0&&Max == negative[i])
-			mode[index++] = -i;
-	}
-
-
-	quicksort(0, index - 1, mode);
-
-	if (index!=1) {
-		if (mode[0] >= 0 && index == 2)
-			return mode[0];
-		
-		return mode[1];
-	}
-	else
-		return mode[0];
-
-
-
-
-
-
+        return mode[1];
+    }
+    else
+        return mode[0];
 }
 
+int main()
+{
+    int N, n, sum = 0, range, mid, avg, Mode;
+    scanf("%d", &N);
 
-int main() {
-	int N, n, sum = 0, range, mid, avg, Mode;
-	scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &num[i]);
 
-	for (int i = 0; i < N; i++) {
-		scanf("%d", &num[i]);
+        sum += num[i];
+        if (num[i] < 0)
+            negative[-num[i]]++;
 
-		sum += num[i];
-		if (num[i] < 0)
-			negative[-num[i]]++;
+        else
+            positive[num[i]]++;
+    }
 
-		else
-			positive[num[i]]++;
-	}
+     quicksort(0, N - 1, num);
+    
+    avg = round((double)sum / N);
+    mid = num[N / 2];
+    Mode = findmode(positive, negative);
+    range = (num[N - 1] - num[0]);
+    printf("%d\n%d\n%d\n%d", avg, mid, Mode, range);
 
-
-	avg = round((double)sum / N);
-	quicksort(0, N - 1, num);
-
-	mid = num[N / 2];
-	Mode = findmode(positive, negative);
-	range = (num[N - 1] - num[0]);
-	printf("%d\n%d\n%d\n%d", avg, mid, Mode, range);
-
-
-	return 0;
+    return 0;
 }
