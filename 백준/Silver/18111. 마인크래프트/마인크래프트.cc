16451bebe22a9d1b50c009dc124bde4224cc9080
COOLHO <coolho123@naver.com>
2023-02-07 22:39:19 +0900

    [Silver II] Title: 마인크래프트, Time: 100 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.cc
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#define vi vector<int>
 using namespace std;
 
 int main()
@@ -9,51 +8,45 @@ int main()
     cin.tie(0);
     cout.tie(0);
     int N, M, input, cnt;
-    long long B, inven;
-    vector<vi> land;
+    int B, inven;
+    vector<int> land;
 
     cin >> N >> M >> B;
 
-    int check = N * M;
+    int size = N * M;
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < size; i++)
     {
-        vector<int> temp;
-        for (int j = 0; j < M; j++)
-        {
-            cin >> input;
-            temp.push_back(input);
-        }
-        land.push_back(temp);
+
+        cin >> input;
+        land.push_back(input);
     }
 
-    long long min = LLONG_MAX;
-    long long maxheight = 0, time = 0, add = 0;
+    int min = INT_MAX;
+    int maxheight = 0, time = 0, add = 0;
 
     for (int h = 0; h < 257; h++)
     {
         time = 0;
         add = 0;
-        
-        for (int i = 0; i < N; i++)
+
+        for (int i = 0; i < size; i++)
         {
-            for (int j = 0; j < M; j++)
+
+            int block = land[i] - h;
+
+            if (block >= 0) // 블록제거
             {
-                int block = land[i][j] - h;
+                time += (2 * block);
+                add += block;
+                cnt++;
+            }
 
-                if (block >= 0) // 블록제거
-                {
-                    time += (2 * block);
-                    add += block;
-                    cnt++;
-                }
-
-                else // 블록 추가
-                {
-                    time -= block;
-                    add += block;
-                    cnt++;
-                }
+            else // 블록 추가
+            {
+                time -= block;
+                add += block;
+                cnt++;
             }
         }
 
