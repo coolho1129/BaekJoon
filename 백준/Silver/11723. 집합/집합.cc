b7a9374395acbe020e528cbd507cba236ab34290
COOLHO <coolho123@naver.com>
2023-02-07 23:54:18 +0900

    [Silver V] Title: 집합, Time: 808 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    cin >> T;
+    int S[20] = {0};
+
+    for (int i = 0; i < T; i++)
+    {
+        string op;
+        int x;
+        cin >> op;
+
+        if (!op.compare("all"))
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                S[i] = 1;
+            }
+            continue;
+        }
+
+        else if (!op.compare("empty"))
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                S[i] = 0;
+            }
+            continue;
+        }
+
+        cin >> x;
+
+        if (!op.compare("toggle"))
+        {
+
+            if (!S[x - 1])
+            {
+                S[x - 1] = 1;
+            }
+
+            else
+            {
+                S[x - 1] = 0;
+            }
+        }
+
+        else if (!op.compare("add"))
+        {
+
+            if (!S[x - 1])
+            {
+                S[x - 1] = 1;
+            }
+        }
+
+        else if (!op.compare("remove"))
+        {
+
+            if (S[x - 1])
+            {
+                S[x - 1] = 0;
+            }
+        }
+        else if (!op.compare("check"))
+        {
+
+            cout << S[x - 1] << "\n";
+        }
+    }
+}
