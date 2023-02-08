2223476249d05d6f6a7950713f2ec471a278f8d0
COOLHO <coolho123@naver.com>
2023-02-08 22:40:07 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 1, Time: 404 ms, Memory: 23508 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+using namespace std;
+
+int tmp[5000000], cnt = 0, result = -1;
+int N, K;
+
+void merge(vi &arr, int start, int mid, int end);
+void merge_sort(vi &arr, int start, int end);
+
+int main()
+{
+
+    cin >> N >> K;
+    vi arr(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    merge_sort(arr, 0, N - 1);
+
+    cout << result;
+}
+
+void merge(vi &arr, int start, int mid, int end)
+{
+
+    int i = start, j = mid + 1, t = 1;
+
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] <= arr[j])
+        {
+            tmp[t++] = arr[i++];
+        }
+        else
+        {
+            tmp[t++] = arr[j++];
+        }
+    }
+
+    while (i <= mid)
+    {
+        tmp[t++] = arr[i++];
+    }
+
+    while (j <= end)
+    {
+        tmp[t++] = arr[j++];
+    }
+
+    i = start, t = 1;
+
+    while (i <= end)
+    {
+        cnt++;
+
+        arr[i++] = tmp[t++];
+        if (cnt == K)
+        {
+            // cout << "K: " << K << " cnt:" << cnt << endl;
+            result = arr[i - 1];
+        }
+    }
+}
+
+void merge_sort(vi &arr, int start, int end)
+{
+    int mid;
+
+    if (start < end)
+    {
+        mid = (start + end) / 2;
+        merge_sort(arr, start, mid);
+        merge_sort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+}
