8bd7a83271b9c9817a3ac8985cd7eb429d0271b8
COOLHO <coolho123@naver.com>
2023-02-11 02:02:55 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -3,18 +3,27 @@ using namespace std;
 
 int main()
 {
-    int N;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N, mod = 15746;
     cin >> N;
 
-    vector<long long> dp(N+1);
+    vector<int> dp(N);
 
-    dp[1] = 1;
-    dp[2] = 2;
+    dp[0] = 1;
+    dp[1] = 2;
 
-    for (int i = 3; i <= N; i++)
+    for (int i = 2; i < N; i++)
     {
-        dp[i] = dp[i - 1] % 15746 + dp[i - 2] % 15746;
+        dp[i] = dp[i - 1] % mod + dp[i - 2] % mod;
     }
 
-    cout << dp[N]%15746;
+    cout << dp[N - 1] % mod;
 }
+
+// 1 1  1개
+// 2 (00) 11 2개 
+// 3 (00)1  111 3개 
+// 4 (00)(00) 11(00) 1111  5개 
+// 5 1(00)(00) 111(00) 11111 8개 
+// 6 (00)(00)(00) (00)(00)11 (00)1111 111111 13개
