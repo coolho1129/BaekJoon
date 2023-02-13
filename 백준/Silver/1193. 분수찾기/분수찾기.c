a991d66ba22128a87f986d3e9e3a0fde8f00b9e0
COOLHO <coolho123@naver.com>
2023-02-14 01:05:16 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.c
+++ 백준/Silver/1193. 분수찾기/분수찾기.c
@@ -1,50 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h> 
+#include <stdio.h>
 
 int main()
 {
-	long long x;
-	int n, m,k = 1,sum=0;
+    long long x;
+    int n = 0, m, k = 1, sum = 0;
 
-	scanf("%lld", &x);
-	if (x == 1)
-		printf("1/1");
-	else
-	{
-		for (int i = 1;; i++)
-		{
-			if (i * (i + 1) / 2 < x && x <=(i + 1) * (i + 2) / 2)
-			{
-				n = i;
-				break;
+    scanf("%lld", &x);
 
-			}
-		}
-		sum = (n+1) * n / 2;
-		int L = x - sum;
-		//printf("%d %d %d\n", n, sum, L);
-		
-		if (n % 2 == 1)
-		{
-			m = n + 1;
-			
-			for (int j = 1; j < L; j++)
-			{
-				k++, m--;
-			}
-			printf("%d/%d", k,m );
-		}
-		else
-		{
-			m = n + 1;
-			for (int j = 1; j < L; j++)
-			{
-				k++, m--;
-			}
-			printf("%d/%d ", m,k);
+    while (!(n * (n + 1) / 2 < x && x <= (n + 1) * (n + 2) / 2))
+    {
+        n++;
+    }
 
+    sum = (n + 1) * n / 2;
+    int L = x - sum;
+    m = n + 1;
 
-		}
+    if (n % 2 == 1)
+    {
 
-	}
+        for (int j = 1; j < L; j++)
+        {
+            k++, m--;
+        }
+        printf("%d/%d", k, m);
+    }
+
+    else
+    {
+        for (int j = 1; j < L; j++)
+        {
+            k++, m--;
+        }
+        printf("%d/%d ", m, k);
+    }
 }
