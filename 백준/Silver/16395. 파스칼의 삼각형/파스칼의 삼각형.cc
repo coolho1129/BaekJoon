10671ca5e31b4be90e8566a9c7f50582f318193d
COOLHO <coolho123@naver.com>
2023-02-22 02:42:21 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Combination[31][31];
+
+void pascal_triangle(int N, int M)
+{
+
+    for (int n = 0; n <= N; n++)
+    {
+        for (int k = 0; k <= n && k <= N; k++)
+        {
+
+            if (n == k || k == 0)
+                Combination[n][k] = 1;
+            else
+                Combination[n][k] = (Combination[n - 1][k - 1] + Combination[n - 1][k]);
+        }
+    }
+
+    cout << Combination[N - 1][M - 1];
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    pascal_triangle(N, M);
+}
