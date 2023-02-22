3b2d66dfa756526ff31dd71ed788b4a341cee607
COOLHO <coolho123@naver.com>
2023-02-23 01:07:22 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string input;
+    cin>>input;
+    cout<<input.size();
+}
