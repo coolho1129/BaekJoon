8b5a72108111d0ce201727d75e462e9ce70831ca
COOLHO <coolho123@naver.com>
2023-03-02 03:35:45 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
@@ -1,17 +1,12 @@
 #include <bits/stdc++.h>
-#define SWAP(a, b, t) \
-    {                 \
-        t = a;        \
-        a = b;        \
-        b = t;        \
-    }
+
 using namespace std;
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int N, M, a, b, c, temp;
+    int N, M, a, b, c;
     cin >> N >> M;
     int *basket = (int *)calloc(N, sizeof(int));
 
@@ -23,8 +18,8 @@ int main()
     for (int i = 0; i < M; i++)
     {
         cin >> a >> b >> c;
-        int idx = 0, begin = a - 1;
-        int tmp[100] = {0};
+        int idx = 0;
+        int tmp[b - a + 1] = {0};
 
         for (int j = c - 1; j < b; j++)
         {
