8f7beea0fc59e217425f08fba67572d36980b15d
COOLHO <coolho123@naver.com>
2023-03-02 03:56:56 +0900

    [Bronze I] Title: 약수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    for (;;)
+    {
+        cin >> N;
+        vector<int> divisor;
+        int sum = 1;
+
+        if (N == -1)
+            break;
+
+        divisor.push_back(1);
+
+        for (int i = 2; i <= N / 2; i++)
+        {
+            if (N % i == 0)
+            {
+                divisor.push_back(i);
+                sum += i;
+            }
+        }
+
+        if (sum == N)
+        {
+            int size = divisor.size();
+
+            cout << N << " "
+                 << "=";
+
+            for (int i = 0; i < size; i++)
+            {
+                cout << " " << divisor[i];
+                if (i != size - 1)
+                    cout << " "
+                         << "+";
+            }
+        }
+        else
+        {
+            cout << N << " is NOT perfect.";
+        }
+        cout << "\n";
+    }
+}
