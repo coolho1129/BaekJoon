3be4b8833c6023544a31cfd253b92a6184147190
COOLHO <coolho123@naver.com>
2023-03-02 02:42:14 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+
+    ios::sync_with_stdio(false);
+
+    cin.tie(NULL);
+
+   
+
+    long N;
+
+    cin>>N;
+
+    cout<<(N*(N-1)*(N-2))/6<<"\n"<<3;
+
+    
+
+}
