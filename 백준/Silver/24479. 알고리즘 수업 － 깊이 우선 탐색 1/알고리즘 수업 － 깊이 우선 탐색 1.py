8a9b9bdc0f82bf364de8429ae8a39d0d49b968d4
COOLHO <coolho123@naver.com>
2023-03-13 06:19:04 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 496 ms, Memory: 73636 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,35 +1,36 @@
 import sys
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 global cnt
 cnt=1
 
-def dfs(visit,graph,start):
+def dfs(visit,graph,start,result):
     global cnt
-    visit[start]=cnt
+    visit[start]=True
+    result[start]=cnt
     graph[start].sort()
-    for i in range(len(graph[start])):
-        next=graph[start][i]
-        if not visit[next]:
+    for next in graph[start]:
+        if visit[next]==False:
             cnt+=1
-            dfs(visit,graph,next)
+            dfs(visit,graph,next,result)
     
     
 def main():
     n,m,start=map(int,input().split())
     graph=[[] for _ in range(n+1)]
-    dfs_visit=[0 for _ in range(n+1)]
+    dfs_visit=[False for _ in range(n+1)]
+    result=[0 for _ in range(n+1)]
     
     for _ in range(m):
         a,b=map(int,input().split())
         graph[a].append(b)
         graph[b].append(a)
     
-    dfs(dfs_visit,graph,start)
+    dfs(dfs_visit,graph,start,result)
     for i in range(n+1):
         if i!=0:
-            print(dfs_visit[i])
+            print(result[i])
     
    
     
