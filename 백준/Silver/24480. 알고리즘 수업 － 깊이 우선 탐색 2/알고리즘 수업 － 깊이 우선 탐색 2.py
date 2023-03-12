082468129f376bebdaf605cab1f6df3925bcb415
COOLHO <coolho123@naver.com>
2023-03-13 06:25:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 504 ms, Memory: 73636 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,42 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+global cnt
+cnt=1
+
+def dfs(visit,graph,start,result):
+    global cnt
+    visit[start]=True
+    result[start]=cnt
+    graph[start].sort(reverse=True)
+    for next in graph[start]:
+        if visit[next]==False:
+            cnt+=1
+            dfs(visit,graph,next,result)
+    
+    
+def main():
+    n,m,start=map(int,input().split())
+    graph=[[] for _ in range(n+1)]
+    dfs_visit=[False for _ in range(n+1)]
+    result=[0 for _ in range(n+1)]
+    
+    for _ in range(m):
+        a,b=map(int,input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    dfs(dfs_visit,graph,start,result)
+    for i in range(n+1):
+        if i!=0:
+            print(result[i])
+    
+   
+    
+main()
+    
+    
+        
+
+
