67c5a68a381499788db4d062618bac6ba0d75a3f
COOLHO <coolho123@naver.com>
2023-03-17 02:39:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 176 ms, Memory: 114484 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,27 +1,19 @@
-from collections import deque
+global count
+count=0
 
-def bfs(x,y,visit,counts,graph):
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, -1, 1]
-    q = deque([(x, y)])
+def dfs(x,y,visit,graph):
     n=len(graph)
-    visit[x][y] = True
-    count = 0
-   
-    while q:
-        x,y=q.popleft()
-        count+=1
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if (0 <= nx < n and 0 <= ny < n and visit[nx][ny]==False and graph[nx][ny]=='1'):
-                q.append((nx, ny))
-                visit[nx][ny] = True
     
-    counts.append(count)
-
+    if(0 <= x < n and 0 <= y < n and visit[x][y]==False and graph[x][y]=='1'):
+        visit[x][y]=True
+        global count
+        count+=1
+        dfs(x-1,y,visit,graph)
+        dfs(x+1,y,visit,graph)
+        dfs(x,y-1,visit,graph)
+        dfs(x,y+1,visit,graph)
+    return count
+        
 def main():
     n=int(input())
     graph = [input() for _ in range(n)]
@@ -32,7 +24,9 @@ def main():
     for i in range(n):
        for j in range(n):
         if(graph[i][j]=='1' and visit[i][j]==False):
-            bfs(i,j,visit,counts,graph)
+           counts.append(dfs(i,j,visit,graph))
+           global count
+           count=0
     
     counts.sort()
     
