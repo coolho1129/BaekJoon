19ebd5c0fd6daf7ba0b7d124a2e9da99b9f7ee4a
COOLHO <coolho123@naver.com>
2023-03-17 01:16:30 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 372 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
@@ -5,20 +5,20 @@ def main():
     n=int(input())
     stack=[]
     result=0
+    
     for _ in range(n):
         h=int(input())
-        
         
         while(len(stack)!=0 and stack[-1][0]<h):
             result+=stack.pop()[1]
             
         size=len(stack)
+        
         if(size!=0 and stack[-1][0]==h):
             if(size!=1):
                 result+=1
             result += stack[-1][1]
             stack[-1][1]+=1
-            
             
         else:
             if (size!=0):
