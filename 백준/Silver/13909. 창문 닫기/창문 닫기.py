3c2f0bbafa4e07ddaf06beebaa0a1c365ad7822a
COOLHO <coolho123@naver.com>
2023-03-28 15:02:37 +0900

    [Silver V] Title: 창문 닫기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.py
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.py
@@ -1,13 +1,1 @@
-import sys,math
-input=sys.stdin.readline
-
-def main():
-    n=int(input())
-    result=0
-    for _ in range(1,int(math.sqrt(n))+1):
-        result+=1
-    print(result) 
-    
-   
-        
-main() 
+print(int((int(input())**0.5)))      
