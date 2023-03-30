0b00bfc66efb294cfe8808aa7f34f5d5829e5382
COOLHO <coolho123@naver.com>
2023-03-30 16:42:22 +0900

    [Silver III] Title: N과 M (3), Time: 2712 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,27 @@
+import sys
+input=sys.stdin.readline
+
+def dfs(n,m,start,visit):
+    if len(visit) == m:
+        for x in visit:
+            print(x, end=' ')
+        print()
+        return
+    
+    
+    for i in range(1, n+1):
+        visit.append(i)
+        dfs(n,m,start,visit)
+        visit.pop()
+        
+def main():
+    n,m = list(map(int,input().split()))
+    visit=[]
+    dfs(n,m,1,visit)
+    
+main()
+ 
+
+
+
+
