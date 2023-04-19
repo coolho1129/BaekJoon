484f072d421780d9d6e91b0694acfee4d38b7377
COOLHO <coolho123@naver.com>
2023-04-20 03:35:23 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.c
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.c
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS 
 #include<stdio.h>
 #include<stdlib.h>
 #define min(a,b) (((a) < (b)) ? (a) : (b))
@@ -12,16 +11,14 @@ void Count_repaint(char** input,int N,int M) {
 						"BWBWBWBW",
 						"WBWBWBWB",
 						"BWBWBWBW" };
-
+    int result = 64;
 	
-	int result = 64;
-	for (int i = 0; i<N-7; i++) {
-		
-		for (int j = 0; j < M - 7; j++) {
+    for(int i = 0; i<N-7; i++){
+		for(int j = 0; j < M - 7; j++){
 			int cnt = 0;
-			for (int k = i; k < i + 8; k++) {
-				for (int l = j; l < j + 8; l++) {
-					if (input[k][l] != chess[k - i][l - j]) {
+			for(int k = i; k < i + 8; k++){
+				for(int l = j; l < j + 8; l++) {
+					if(input[k][l] != chess[k - i][l - j]){
 						cnt++;
 					}
 				}
@@ -29,9 +26,7 @@ void Count_repaint(char** input,int N,int M) {
 			cnt = min(cnt, 64 - cnt);
 			result = min(result, cnt);
 		}
-
-
-	}
+    }
 
 	printf("%d", result);
 }
@@ -40,18 +35,16 @@ int main() {
 	int N, M;
 	scanf("%d %d", &N,&M);
 	char** input = calloc(N, sizeof(char*));
-	for (int i = 0; i < N; i++) {
+	
+    for (int i = 0; i < N; i++) {
 		input[i] = calloc(M+1, sizeof(char));
 	}
 	
-
-	for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
 		scanf("%s", input[i]);
 	}
 	
 	Count_repaint(input, N, M);
-
-	
-
-	return 0;
+    
+    return 0;
 }
