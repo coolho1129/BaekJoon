c4244246e066ee148ef08ece2c548dac7b384e94
COOLHO <coolho123@naver.com>
2023-04-20 00:57:57 +0900

    [Silver III] Title: 어린 왕자, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,11 +1,12 @@
 import sys
+input=sys.stdin.readline
 
-T = int(sys.stdin.readline())
+T = int(input())
 for _ in range(T):
-    x1,y1,x2,y2 = map(int, sys.stdin.readline().split())
-
-    n = int(sys.stdin.readline())
+    x1,y1,x2,y2 = map(int, input().split())
+    n = int(input())
     cnt = 0
+    
     for i in range(n):
         x,y,r = map(int, sys.stdin.readline().split())
         d1 = ((x1-x)**2 + (y1-y)**2)**0.5
@@ -13,6 +14,5 @@ for _ in range(T):
        
         if (d1 < r) ^ (d2 < r):
             cnt += 1
-        
-
+    
     print(cnt)
