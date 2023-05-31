81951b6fdba7c8d0dd7a2b54aba4502d4dfa2d9b
COOLHO <coolho123@naver.com>
2023-05-31 16:37:25 +0900

    [Silver I] Title: 정수 삼각형, Time: 120 ms, Memory: 41824 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -11,13 +11,13 @@ def main():
     for i in range(1,n):
         for j in range(0,i+1):
            if(j==0):
-               dp[i][j]=dp[i-1][j]+triangle[i][j]
+                dp[i][j]=dp[i-1][j]+triangle[i][j]
            
            elif(j==i):
-               dp[i][j]=dp[i-1][j-1]+triangle[i][j]
+                dp[i][j]=dp[i-1][j-1]+triangle[i][j]
            
            else:
-               dp[i][j]+=max(dp[i-1][j-1],dp[i-1][j])+triangle[i][j]
+                dp[i][j]+=max(dp[i-1][j-1],dp[i-1][j])+triangle[i][j]
            
         
     print(max(dp[n-1]))
