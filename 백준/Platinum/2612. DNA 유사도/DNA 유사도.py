8032008b2c38233a7f1adb20b987c6e393af02b8
COOLHO <coolho123@naver.com>
2023-06-10 02:27:30 +0900

    [Platinum IV] Title: DNA 유사도, Time: 688 ms, Memory: 47940 KB -BaekjoonHub

--- 백준/Platinum/2612. DNA 유사도/DNA 유사도.py
+++ 백준/Platinum/2612. DNA 유사도/DNA 유사도.py
@@ -1,24 +1,21 @@
 import sys
 input=sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-def backTrack(a,b,i,j,score,back,res):
+def backTrack(i,j,score,back,res):
     if back[i][j]=='S':
         res[0]=i
         res[1]=j
         return 
     else:
         if back[i][j]=='U':
-            backTrack(a,b,i-1,j,score,back,res)
+            backTrack(i-1,j,score,back,res)
     
         elif back[i][j]=='L':
-            backTrack(a,b,i,j-1,score,back,res)
+            backTrack(i,j-1,score,back,res)
     
         else:
-            backTrack(a,b,i-1,j-1,score,back,res)
+            backTrack(i-1,j-1,score,back,res)
    
-    
-
 def main():
     x_len=int(input())
     x=input().rstrip()
@@ -31,8 +28,6 @@ def main():
     score_matrix=[[0 for _ in range(y_len+1)] for _ in range(x_len+1)]
     backtrack_matrix=[["S" for _ in range(y_len+1)] for _ in range(x_len+1)]
     
-
-
     for col in range(1,x_len+1):
         for row in range(1,y_len+1):
             s=miss
@@ -69,7 +64,6 @@ def main():
     #           print("%5s"%(backtrack_matrix[i][j]),end=" ")
     #     print()
         
-    
     findmax=0
     maxi=0
     maxj=0
@@ -80,25 +74,11 @@ def main():
             maxi=i
     
     maxj=score_matrix[maxi].index(findmax)
-   
-   
-    backTrack(x,y,maxi,maxj,score_matrix,backtrack_matrix,res)
+    backTrack(maxi,maxj,score_matrix,backtrack_matrix,res)
+    
     print(findmax)
-    # print(maxi,maxj)
-    # print(res[0],res[1])
-    # print()
-    # print(x)
-    # print(y)
-    if(res[0]<0):
-        res[0]=0
-    if(res[1]<0):
-        res[1]=0
-    for i in range(res[0],maxi):
-        print(x[i],end="")
-    print()
-    for j in range(res[1],maxj):
-        print(y[j],end="")
-        
+    print(x[res[0]:maxi])
+    print(y[res[1]:maxj])
     
         
 main()
