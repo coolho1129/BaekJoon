17e57fa248134afef964284cb6c26707e926c294
COOLHO <coolho123@naver.com>
2023-06-10 23:56:36 +0900

    [Gold V] Title: 강의실 배정, Time: 296 ms, Memory: 61296 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+import heapq
+
+
+
+def main():
+    n = int(input().strip())
+    lecture=[]
+    for _ in range(n):
+        start,finish=map(int,input().split())
+        lecture.append((start,finish))
+  
+    lecture.sort()
+    prior_que=[]
+
+    heapq.heappush(prior_que,lecture[0][1])
+    for i in range(1,n):
+        if lecture[i][0] < prior_que[0]:
+            heapq.heappush(prior_que,lecture[i][1])
+        else:
+            heapq.heappop(prior_que)
+            heapq.heappush(prior_que,lecture[i][1])
+    print(len(prior_que))
+            
+    
+    
+        
+
+main()
