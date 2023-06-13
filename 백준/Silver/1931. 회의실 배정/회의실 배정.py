1f8af1c32b80eb4ae34d9d2bc4777bf690a08d0d
COOLHO <coolho123@naver.com>
2023-06-13 16:01:59 +0900

    [Silver I] Title: 회의실 배정, Time: 236 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -5,15 +5,15 @@ sys.setrecursionlimit(10**6)
 def activate(meeting):
     n=len(meeting)
     meeting.sort(key=lambda x:(x[1],x[0]))
-    A=[0]
+    cnt=1
     k=0
     
     
     for i in range(1,n):
         if(meeting[i][0]>=meeting[k][1]):
-            A.append(i)
+            cnt+=1
             k=i
-    return len(A)
+    return cnt
 
 
 def main():
