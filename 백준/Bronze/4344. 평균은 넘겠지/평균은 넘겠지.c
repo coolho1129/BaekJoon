d7feb319d53acc52244485b732d7ac4e1b6a4bd3
COOLHO <coolho123@naver.com>
2023-06-25 17:00:33 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 0 ms, Memory: 1788 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.c
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.c
@@ -1,41 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h> 
+#include <stdio.h>
+#include <math.h>
 
-
-main()
+int main()
 {
-	int c;
-	int n[1000], cnt[1000] = {0};
-	double score[100][1000] = { 0 }, sum[100] = { 0 }, avg[100] = { 0 }, percent[100] = { 0 };
-	scanf("%d", &c);
+    int c;
+    int n[1000], cnt[1000] = {0};
+    double score[100][1000] = {0}, sum[100] = {0}, avg[100] = {0}, percent[100] = {0};
+    scanf("%d", &c);
 
-	for (int i = 0; i < c; i++)
-	{
-		scanf("%d", &n[i]);
-		for (int j = 0; j < n[i]; j++)
-		{
-			scanf("%lf", &score[i][j]);
-			sum[i] += score[i][j];
-		}
-		avg[i] = sum[i] / n[i];
-	}
-	
-	
-	for (int i = 0; i < c; i++)
-	{
-		for (int j = 0; j < n[i]; j++)
-		{
-			if (avg[i] < score[i][j])
-				cnt[i]++;
-		}
-		percent[i] = ((double)cnt[i] / n[i]) * 100;
-	}
+    for (int i = 0; i < c; i++)
+    {
+        scanf("%d", &n[i]);
+        for (int j = 0; j < n[i]; j++)
+        {
+            scanf("%lf", &score[i][j]);
+            sum[i] += score[i][j];
+        }
+        avg[i] = sum[i] / n[i];
+    }
 
-	for (int i = 0; i < c; i++)
-	{
-		printf("%0.3lf%\n", percent[i]);
-		
-	}
-	
-	
+    for (int i = 0; i < c; i++)
+    {
+        for (int j = 0; j < n[i]; j++)
+        {
+            if (avg[i] < score[i][j])
+                cnt[i]++;
+        }
+        percent[i] = round(((double)cnt[i] / n[i]) * 100000) / 1000;
+    }
+
+    for (int i = 0; i < c; i++)
+    {
+        printf("%0.3lf%\n", percent[i]);
+    }
 }
