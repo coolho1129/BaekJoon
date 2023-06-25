5f40072dd2b8df5cd45f39aa3fdbe8f289a4dd86
COOLHO <coolho123@naver.com>
2023-06-25 17:06:34 +0900

    [Silver III] Title: 터렛, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.c
+++ 백준/Silver/1002. 터렛/터렛.c
@@ -1,14 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> 
 #include <math.h>
-#include <string.h>
 #include <stdlib.h>
 #include <float.h>
 
-
 void cross_point_cnt(int x1, int y1, int r1, int x2, int y2, int r2)
 {
-
 	double point_distance = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
 	double radius_sum = (double)r1 +r2;
 	double radius_diff = fabs((double)r1 - r2);
@@ -24,13 +21,11 @@ void cross_point_cnt(int x1, int y1, int r1, int x2, int y2, int r2)
 		else
 			printf("2\n");
 	}
-	
-	
 }
+
 int main()
 {
 	int T, x1, y1, r1, x2, y2, r2;
-	
 	scanf("%d", &T);
 	
 	for (int i = 0; i < T; i++)
@@ -38,6 +33,5 @@ int main()
 		scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
 		cross_point_cnt(x1, y1, r1, x2, y2, r2);
 	}
-	
 
 }
