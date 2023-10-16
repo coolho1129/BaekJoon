d92895235f257701fcb491caae8c9f4b9d490965
CoolHo(김찬호) <coolho123@naver.com>
2023-10-16 20:30:14 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 952 ms, Memory: 123400 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,5 +1,31 @@
-n=int(input())
-a=[int(input()) for _ in range(n)]
-a.sort()
-for res in a:
-    print(res)
+import random
+
+def partition3Way(a, lo, hi):
+    if (hi <= lo): return
+    v = a[lo]
+    lt, gt = lo, hi   
+    i = lo
+    while i <= gt:
+        if a[i] < v:
+            a[lt], a[i] = a[i], a[lt]  # Swap
+            lt, i = lt+1, i+1
+        elif a[i] > v:
+            a[gt], a[i] = a[i], a[gt]  # Swap
+            gt = gt-1
+        else: i = i+1
+
+    partition3Way(a, lo, lt-1)
+    partition3Way(a, gt+1, hi)
+
+def quickSort3Way(a):
+    random.shuffle(a)
+
+    partition3Way(a, 0, len(a)-1)
+    return a
+
+if __name__ == "__main__":
+    n=int(input())
+    a=[int(input()) for _ in range(n)]
+    res=quickSort3Way(a)
+    for r in res:
+        print(r)
