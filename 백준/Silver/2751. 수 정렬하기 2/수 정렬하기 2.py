a78487c67ce98b6b751b933211e4f28848b92b6c
CoolHo(김찬호) <coolho123@naver.com>
2023-10-20 05:50:50 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4652 ms, Memory: 79404 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,35 +1,34 @@
-import sys
+import sys,random
 input=sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-def merge(a,aux,lo,mid,hi):
-    for k in range(lo,hi+1):
-        aux[k]=a[k]
+def partition(a,lo,hi):
+    i,j=lo+1,hi
+    while(True):
+        while(i<=hi and a[i]<a[lo]):i+=1
+        while(j>=lo+1 and a[lo]<a[j]):j-=1
+        if(j<=i):break
+        a[i],a[j]=a[j],a[i]
+        i,j=i+1,j-1
         
-    i,j=lo,mid+1
-    for k in range(lo,hi+1):
-        if i>mid: a[k],j=aux[j],j+1
-        elif j>hi: a[k],i=aux[i],i+1
-        elif aux[i]<=aux[j]:a[k],i=aux[i],i+1
-        else: a[k],j=aux[j],j+1
-            
-def dividedNmerge(a,aux,lo,hi):
-    if(hi<=lo): return a
-    mid=(lo+hi)//2
-    dividedNmerge(a,aux,lo,mid)
-    dividedNmerge(a,aux,mid+1,hi)
-    merge(a,aux,lo,mid,hi)
+    a[lo],a[j]=a[j],a[lo]
+    return j
+
+def dividedNpartition(a,lo,hi):
+    if(hi<=lo):return
+    j=partition(a,lo,hi)
+    dividedNpartition(a,lo,j-1)
+    dividedNpartition(a,j+1,hi)
     
-def mergesort(a):
-    aux=[None]*len(a)
-    dividedNmerge(a,aux,0,len(a)-1)
+def quicksort(a):
+    random.shuffle(a)
+    dividedNpartition(a,0,len(a)-1)
     
-    
+
 n=int(input())
 a=[0]*n
 for i in range(n):
     a[i]=int(input())
-mergesort(a)
+quicksort(a)
 
 for v in a:
     print(v)
