74ebf5eb1bf1c6c01272d08ccf979f47e6d1515d
CoolHo(김찬호) <coolho123@naver.com>
2023-10-19 11:54:15 +0900

    [Gold IV] Title: 사이클 게임, Time: 512 ms, Memory: 50548 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -6,29 +6,20 @@ def root(ids,i):
     return i
     
 
-def union(size,ids,p,q):
+def union(ids,p,q):
    
     idx1,idx2=root(ids,p),root(ids,q)
     
     if p==q or idx1==idx2:
         return True
-
-    if size[idx1]<=size[idx2]:
-        ids[idx1]=ids[idx2]
-        size[idx2]+=size[idx1]
-   
-    else:
-        ids[idx2]=ids[idx1]
-        size[idx1]+=size[idx2]
-        
-  
+    ids[idx2]=ids[idx1]
+      
 def main():
     n,m=map(int,input().split())
-    size=[1 for _ in range(n)]
     ids=[i for i in range(n)]
     for i in range(m):
         p,q=map(int,input().split())
-        if(union(size,ids,p,q)):
+        if(union(ids,p,q)):
             print(i+1)
             return 
 
