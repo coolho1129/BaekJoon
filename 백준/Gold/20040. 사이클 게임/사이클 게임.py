a009728ae1b52da536e840c8ba0ff9fbeb90a794
CoolHo(김찬호) <coolho123@naver.com>
2023-10-19 11:44:25 +0900

    [Gold IV] Title: 사이클 게임, Time: 672 ms, Memory: 54620 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,41 @@
+import sys
+input=sys.stdin.readline
+
+def root(ids,i):
+    while i !=ids[i]: i=ids[i]
+    return i
+    
+
+def union(size,ids,p,q):
+   
+    idx1,idx2=root(ids,p),root(ids,q)
+    
+    if p==q or idx1==idx2:
+        return size[idx1]
+
+    if size[idx1]<=size[idx2]:
+        ids[idx1]=ids[idx2]
+        size[idx2]+=size[idx1]
+        return size[idx2]
+
+    else:
+        ids[idx2]=ids[idx1]
+        size[idx1]+=size[idx2]
+        return size[idx1]
+    
+def connected(ids,p,q):
+    return root(ids,p)==root(ids,q)
+    
+def main():
+    n,m=map(int,input().split())
+    size=[1 for _ in range(n)]
+    ids=[i for i in range(n)]
+    for i in range(m):
+        p,q=map(int,input().split())
+        if(connected(ids,p,q)):
+            print(i+1)
+            return 
+        union(size,ids,p,q)
+    print(0)
+       
+main()
