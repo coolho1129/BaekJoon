edf3d9b48ff10764c81b4e46917835c420ebf9f5
CoolHo(김찬호) <coolho123@naver.com>
2023-12-17 19:48:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 720 ms, Memory: 73068 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -9,14 +9,9 @@ def main():
         for j in range(n):
             partsum[i+1][j+1]=arry[j]+partsum[i][j+1]+partsum[i+1][j]-partsum[i][j]
 
-    res=0
     for _ in range(m):
         x1,y1,x2,y2=map(int,input().split())
-        res=partsum[x2][y2]-partsum[x1-1][y2]-partsum[x2][y1-1]+partsum[x1-1][y1-1]
-
-        print(res)
-    
+        print(partsum[x2][y2]-partsum[x1-1][y2]-partsum[x2][y1-1]+partsum[x1-1][y1-1])
    
 if __name__ == "__main__":
     main()
-
