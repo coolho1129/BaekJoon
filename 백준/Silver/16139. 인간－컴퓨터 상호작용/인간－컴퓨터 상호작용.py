1fa36751e0c8635874084bed86a299bc6a5a3b3c
CoolHo(김찬호) <coolho123@naver.com>
2024-07-31 23:15:52 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 128 ms, Memory: 34796 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -7,32 +7,25 @@ input = sys.stdin.readline
 def main():
     string = input().strip()
     n = int(input().strip())
-    dp = [[0 for _ in range(len(string))] for _ in range(26)]
-    ls = [0]*n
-    rs = [0]*n
-    alphas = [0]*n
-    for i in range(n):
+    dp = [[0 for _ in range(len(string) + 1)] for _ in range(26)]
+    ls = []
+    rs = []
+    alphas = []
+    for _ in range(n):
         command = input().strip()
         a, l, r = command.split()
-        alphas[i] = a
-        ls[i] = int(l)
-        rs[i] = int(r)
+        alphas.append(a)
+        ls.append(int(l))
+        rs.append(int(r))
 
-    for i in range(26):
-        ch = chr(i+97)
-        cnt = 0
-        for j in range(len(string)):
-            if string[j] == ch:
-                cnt+=1
-            dp[i][j] = cnt
+    for a in set(alphas):
+        idx = ord(a) - 97
+        for i in range(len(string)):
+            dp[idx][i+1] = dp[idx][i] + (1 if string[i] == a else 0)
 
     for a, r, l in zip(alphas, rs, ls):
         idx = ord(a) - 97
-        l-=1
-        if l==-1:
-            print(dp[idx][r])
-        else:
-            print(dp[idx][r] - dp[idx][l])
+        print(dp[idx][r + 1] - dp[idx][l])
 
 
 if __name__ == "__main__":
