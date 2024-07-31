bb58296ff9265dfd1803450b7e4e76865e38d819
CoolHo(김찬호) <coolho123@naver.com>
2024-07-31 23:12:11 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 452 ms, Memory: 173204 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -7,18 +7,32 @@ input = sys.stdin.readline
 def main():
     string = input().strip()
     n = int(input().strip())
-    for _ in range(n):
+    dp = [[0 for _ in range(len(string))] for _ in range(26)]
+    ls = [0]*n
+    rs = [0]*n
+    alphas = [0]*n
+    for i in range(n):
         command = input().strip()
         a, l, r = command.split()
-        cnt = 0
-        l = int(l)
-        r = int(r)
-        temp = string[l : r + 1]
-        for alpha in temp:
-            if alpha == a:
-                cnt += 1
+        alphas[i] = a
+        ls[i] = int(l)
+        rs[i] = int(r)
 
-        print(cnt)
+    for i in range(26):
+        ch = chr(i+97)
+        cnt = 0
+        for j in range(len(string)):
+            if string[j] == ch:
+                cnt+=1
+            dp[i][j] = cnt
+
+    for a, r, l in zip(alphas, rs, ls):
+        idx = ord(a) - 97
+        l-=1
+        if l==-1:
+            print(dp[idx][r])
+        else:
+            print(dp[idx][r] - dp[idx][l])
 
 
 if __name__ == "__main__":
