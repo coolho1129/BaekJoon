8829247b7a78deef7568f6839b399fb10b0dc871
CoolHo(김찬호) <coolho123@naver.com>
2025-01-01 15:10:01 +0900

    [Silver III] Title: 1, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,23 @@
+import sys
+
+def sol(n):
+    x = 1
+    res = 1
+    while True:
+        if x % n == 0:
+            break
+        x *= 10
+        x += 1
+        res += 1
+
+    print(res)
+
+
+def main():
+    lines = sys.stdin.readlines()
+    for n in lines:
+        sol(int(n))
+
+
+if __name__ == "__main__":
+    main()
