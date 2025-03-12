173c4c84638eb94571e345112c4638e32510694b
Chanho Kim <coolho123@naver.com>
2025-03-13 04:26:40 +0900

    [Bronze I] Title: FizzBuzz, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -1,23 +1,35 @@
-n1 = input()
-n2 = input()
-n3 = input()
+import sys
+input=sys.stdin.readline
+   
+def main():
+    n1 = input().strip()
+    n2 = input().strip()
+    n3 = input().strip()
 
-if n1 == "FizzBuzz":
-    print("Fizz")
-    
-elif n1 == "Fizz":
-    num = int(n2) if n2.isdigit() else int(n3)
-    ans = num + (3 - (1 if n2.isdigit() else 1))
-    print("FizzBuzz" if ans % 5 == 0 else "Fizz")
-    
-elif n1 == "Buzz":
-    num = int(n2) if n2.isdigit() else int(n3)
-    ans = num + (3 - (1 if n2.isdigit() else 2))
-    print("Fizz" if ans % 3 == 0 else ans)
-    
-else:
-    ans = int(n1) + 3
-    print("FizzBuzz" if ans % 15 == 0 else "Fizz" if ans % 3 == 0 else "Buzz" if ans % 5 == 0 else ans)
-
-
+    if n1 == "FizzBuzz": # 15배수
+        print("Fizz")
         
+    elif n1 == "Fizz": #3배수
+        num = int(n2) if n2.isdigit() else int(n3)
+        ans = num + (3 - (1 if n2.isdigit() else 2))
+        print("FizzBuzz" if ans % 5 == 0 else "Fizz")
+        
+    elif n1 == "Buzz": #5배수
+        num = int(n2) if n2.isdigit() else int(n3)
+        ans = num + (3 - (1 if n2.isdigit() else 2))
+        print("Fizz" if ans % 3 == 0 else ans)
+        
+    else:
+        ans = int(n1) + 3
+        if ans % 15 == 0:
+            print("FizzBuzz")
+        elif ans % 5 == 0:
+            print("Buzz")
+        elif ans % 3 == 0:
+            print("Fizz")
+        else:
+            print(ans)
+   
+    
+if __name__ == '__main__':
+    main()
