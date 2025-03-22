74996ecd02d1fd1e0eb179f937925d202d7e56f7
Chanho Kim <coolho123@naver.com>
2025-03-23 03:12:25 +0900

    [Gold I] Title: 기벡을 안배운다고?, Time: 716 ms, Memory: 119412 KB -BaekjoonHub

--- 백준/Gold/28132. 기벡을 안배운다고？/기벡을 안배운다고？.py
+++ 백준/Gold/28132. 기벡을 안배운다고？/기벡을 안배운다고？.py
@@ -1,32 +1,44 @@
-import sys
+import sys,math
 from collections import defaultdict
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
+def gcd(a, b) :
+  while b :
+    a, b = b, a%b
+  return a
 
-num_points = int(input())
-orthogonal_counter = defaultdict(int)
-zero_vector_count = 0
-orthogonal_pairs_count = 0
-
-for _ in range(num_points):
-    x, y = map(int, input().split())
-    if x == 0 and y == 0:
-        zero_vector_count += 1
-        continue
+def normalize(x, y):
     if x == 0:
         y = 1
+        
     elif y == 0:
         x = 1
+    
     else:
         gcd_value = gcd(abs(x), abs(y))
         x, y = x // gcd_value, y // gcd_value
-    orthogonal_pairs_count += orthogonal_counter[(x, y)]
-    orthogonal_counter[(-y, x)] += 1
-    orthogonal_counter[(y, -x)] += 1
+    
+    return (x, y)
 
-result = orthogonal_pairs_count + zero_vector_count * (num_points - zero_vector_count) + (zero_vector_count * (zero_vector_count - 1) // 2)
-print(result)
+def main():
+    num_points = int(input())
+    orthogonal_counter = defaultdict(int)
+    zero_vector_count = 0
+    orthogonal_pairs_count = 0
+
+    for _ in range(num_points):
+        x, y = map(int, input().split())
+        if x == 0 and y == 0:
+            zero_vector_count += 1
+            continue
+        x, y = normalize(x, y)
+        
+        orthogonal_pairs_count += orthogonal_counter[(x, y)]
+        orthogonal_counter[(-y, x)] += 1
+        orthogonal_counter[(y, -x)] += 1
+
+    result = orthogonal_pairs_count + zero_vector_count * (num_points - zero_vector_count) + (zero_vector_count * (zero_vector_count - 1) // 2)
+    print(result)
+            
+if __name__ == "__main__":
+    main()
